b546de5801b2c976f82508b40d5ef1cca310b475
Shikhar Khandelwal <skykunnu@gmail.com>
2025-04-03 11:11:36 +0530

    change axios baseURL

--- Front/src/axiosConfig.js
+++ Front/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/profile"
+  baseURL: "https://mapifyme.onrender.com/profile"
 });
 
 export default instance;
